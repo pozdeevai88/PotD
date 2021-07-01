24607318575113c3b169b494997fa343c37d7147
Aleksey <alexcorpetf@gmail.com>
2021-07-01 21:54:08 +1000

    BottomSheet
    Выводим пояснение к картинке в BottomSheet

--- app/src/main/java/ru/geekbrains/potd/ui/main/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/geekbrains/potd/ui/main/PictureOfTheDayFragment.kt
@@ -3,13 +3,16 @@ package ru.geekbrains.potd.ui.main
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import coil.api.load
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import ru.geekbrains.potd.PictureOfTheDayData
 import ru.geekbrains.potd.R
 import ru.geekbrains.potd.databinding.MainFragmentBinding
@@ -18,7 +21,7 @@ class PictureOfTheDayFragment : Fragment() {
 
     private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
-    //Ленивая инициализация модели
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
     private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProviders.of(this).get(PictureOfTheDayViewModel::class.java)
     }
@@ -33,23 +36,31 @@ class PictureOfTheDayFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
         binding.layoutInput.setEndIconOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
+                data =
+                    Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
             })
         }
     }
 
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel.getData().observe(viewLifecycleOwner, { renderData(it) })}
+        viewModel.getData().observe(viewLifecycleOwner, { renderData(it) })
+    }
 
     private fun renderData(data: PictureOfTheDayData) {
-
         when (data) {
             is PictureOfTheDayData.Success -> {
                 val serverResponseData = data.serverResponseData
                 val url = serverResponseData.url
+                val explanation = serverResponseData.explanation
                 if (url.isNullOrEmpty()) {
                     Toast.makeText(context, "EMPTY LINK", Toast.LENGTH_SHORT).show()
                 } else {
@@ -59,10 +70,15 @@ class PictureOfTheDayFragment : Fragment() {
                         placeholder(R.drawable.ic_no_photo_vector)
                     }
                 }
+                if (!explanation.isNullOrEmpty()) {
+                    view?.findViewById<TextView>(R.id.bottom_sheet_description)?.text = explanation
+                }
             }
 
-            is PictureOfTheDayData.Loading -> { }
-            is PictureOfTheDayData.Error -> { }
+            is PictureOfTheDayData.Loading -> {
+            }
+            is PictureOfTheDayData.Error -> {
+            }
         }
     }
 
